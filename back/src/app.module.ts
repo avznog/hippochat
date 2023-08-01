38fbf78525ab53458b9100ea455b1c7a0f2b130b
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-08-01 13:16:36 +0200

    🗃️ setup database config

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: +process.env.POSTGRES_PORT
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
