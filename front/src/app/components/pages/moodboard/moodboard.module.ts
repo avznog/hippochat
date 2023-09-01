b28106f697c0a0baac759ca069bb6a1cf4bc6e92
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-08-31 22:23:43 -0700

    ✨ add calendars emoji

--- front/src/app/components/pages/moodboard/moodboard.module.ts
+++ front/src/app/components/pages/moodboard/moodboard.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { EmojisCalendarComponent } from '../../moodboard/emojis-calendar/emojis-calendar.component';
+import { MoodboardMonthComponent } from '../../moodboard/moodboard-month/moodboard-month.component';
+import { MoodboardTodayComponent } from '../../moodboard/moodboard-today/moodboard-today.component';
 import { MoodboardPageRoutingModule } from './moodboard-routing.module';
 import { MoodboardPage } from './moodboard.page';
-import { MoodboardTodayComponent } from '../../moodboard/moodboard-today/moodboard-today.component';
-import { MoodboardMonthComponent } from '../../moodboard/moodboard-month/moodboard-month.component';
 
 
 @NgModule({
@@ -16,6 +17,6 @@ import { MoodboardMonthComponent } from '../../moodboard/moodboard-month/moodboa
     IonicModule,
     MoodboardPageRoutingModule,
   ],
-  declarations: [MoodboardPage, MoodboardTodayComponent, MoodboardMonthComponent]
+  declarations: [MoodboardPage, MoodboardTodayComponent, MoodboardMonthComponent, EmojisCalendarComponent]
 })
 export class MoodboardPageModule {}
--- front/src/app/services/daysEmojis/days-emojis.service.ts
+++ front/src/app/services/daysEmojis/days-emojis.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { CreateDaysEmojisDto } from 'src/app/dto/daysEmojis/create-days-emojis.dto';
@@ -11,6 +11,8 @@ export class DaysEmojisService {
 
   myTodaysEmoji?: DaysEmoji;
   myMatesTodaysEmoji?: DaysEmoji;
+
+  allMyMonthly = new Map<string, DaysEmoji>();
   constructor(
     private http: HttpClient
   ) { }
@@ -27,6 +29,17 @@ export class DaysEmojisService {
     this.http.post<DaysEmoji>(`days-emojis/create-today`, createDaysEmojisDto).subscribe(todayDayEmoji => {
       this.myTodaysEmoji = todayDayEmoji;
     })
+  }
+
+  async getAllMyMonthly(date: Date) {
+    let httpPrams = new HttpParams();
+    httpPrams = httpPrams.append("date", date.toISOString());
+     this.http.get<DaysEmoji[]>(`days-emojis/all-my-monthly`, { params: httpPrams }).subscribe(daysEmojis => {
+      daysEmojis.forEach(dayEmoji => {
+        this.allMyMonthly.set(new Date(dayEmoji.date).getDate().toString(), dayEmoji);
+      })
+     })
+    console.log(this.allMyMonthly)
   }
 
 }
