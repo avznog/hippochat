1fa5c91d8b00d488eb99b6ff24d6e85e8bd34276
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-09-17 01:14:41 -0700

    🚧 pcitures are now saved in 3 qualities

--- back/src/minio/minio.service.ts
+++ back/src/minio/minio.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as Minio from "minio";
+import * as sharp from 'sharp';
 
 @Injectable()
 export class MinioService {
@@ -23,6 +24,21 @@ export class MinioService {
 
   async uploadFile(path: string, file: Express.Multer.File) {
     try {
+      const filename = path.split("/").pop()
+      const arrayPath = path.split("/");
+      arrayPath.pop();
+      arrayPath.pop();
+      // ? uploading the file in 3 different qualities : small (80x100), medium (320,320) and original.
+      // ! when loading the file from the front, the browser should load the smallest quality, display it,
+      // ! and behind that load the original, and display it on top when it is loaded
+
+      // ? resizing
+      const smallFile = await sharp(file.buffer).resize(80, 100, { fit: 'inside' });
+      const mediumFile = await sharp(file.buffer).resize(320, 320, { fit: 'inside' });
+
+      // ? puting on minio
+      this.minioClient.putObject("hippochat", `${arrayPath.join("/")}/80x100/${filename}`, smallFile);
+      this.minioClient.putObject("hippochat", `${arrayPath.join("/")}/320x320/${filename}`, mediumFile);
       return await this.minioClient.putObject("hippochat", path, file.buffer);
     } catch (error) {
 
@@ -32,6 +48,7 @@ export class MinioService {
   async generateUrl(path: string): Promise<string> {
     try {
       return await this.minioClient.presignedGetObject("hippochat", path)
+      // return await this.minioClient.presignedGetObject("hippochat", path.replace("original", "80x120"))
     } catch (error) {
       console.log(error)
       return null;
