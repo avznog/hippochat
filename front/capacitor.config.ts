f0c5d72de097cf4d94aab8f1d57260ec4db6ea53
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-10-22 22:16:04 -0700

    🚧 wip

--- front/capacitor.config.ts
+++ front/capacitor.config.ts
@@ -6,13 +6,16 @@ const config: CapacitorConfig = {
   appName: 'Hippochat',
   webDir: 'www',
   server: {
+    // ? when building for xcode, we need
+    // ? androidScheme: 'https',
+    // ? no hostname and no url
     androidScheme: 'https',
     // url: "http://192.168.1.19:8100",
     // url: "http://172.20.10.3:8100",
     // url: "http://192.168.1.15:8100",
     // url: "http://10.221.14.108:8100",
     // url: "http://10.142.40.165:8100",
-    url: "http://192.168.7.191:8100",
+    // url: "http://192.168.7.191:8100",
     cleartext: true,
     // hostname: "bgonzva.com"
   },
--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PublicProfileService } from './services/publicProfile/public-profile.service';
 import { SocketBatteryService } from './services/sockets/socket-battery/socket-battery.service';
+import { ActionPerformed, PushNotificationSchema, PushNotifications, Token } from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-root',
@@ -13,5 +14,49 @@ export class AppComponent {
     private readonly socketBatteryService: SocketBatteryService
   ) {
     this.publicProfileService.setPrimaryColorOnLogin();
+  }
+
+  ngOnInit() {
+    console.log('Initializing HomePage');
+
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    // On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: Token) => {
+        alert('Push registration success, token: ' + token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 }
