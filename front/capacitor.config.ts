37d7a4ce423c8646ebda15abf859aee588507393
Benjamin Gonzva <bgonzva@juniorisep.com>
2023-11-01 00:18:03 -0700

    🍱 update assets + app icon

--- front/capacitor.config.ts
+++ front/capacitor.config.ts
@@ -13,7 +13,7 @@ const config: CapacitorConfig = {
     // url: "http://192.168.1.15:8100",
     // url: "http://10.221.14.108:8100",
     // url: "http://10.142.40.165:8100",
-    url: "http://192.168.7.191:8100",
+    // url: "http://192.168.7.191:8100",
     cleartext: true,
     // hostname: "bgonzva.com"
   },
